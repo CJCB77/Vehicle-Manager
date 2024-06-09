919f1cf87ee175145fd4668c3c9cdb77402919e3
Johnny Coral <coral.johnny.2@gmail.com>
2024-06-09 15:59:23 -0500

    Added table for found vehicle

--- vehicle_manager/vehicle/views.py
+++ vehicle_manager/vehicle/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Vehicle
+from django.db.models import Q
+
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
         search_query = request.POST.get('search_input')
-        vehicle = Vehicle.objects.filter(pk=search_query).first()
-        if vehicle:
-            context = {'vehicle': vehicle}
+        if search_query.isdigit():
+            vehicle = Vehicle.objects.filter(pk=search_query).first()
         else:
-            context = {'error_msg': 'No vehicle found with that ID. Please try again.'}
+            vehicle = Vehicle.objects.filter(serial_number=search_query).first()
+
+        context = {'vehicle': vehicle} if vehicle else {'error_msg': 'No vehicle found.'}
         return render(request, 'vehicle/index.html', context)
     return render(request, 'vehicle/index.html')
